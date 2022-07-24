c78e2f656e4c12806e4a7a66c5542fc22b49db48
Anton Pavlov <dharlequin@gmail.com>
2022-07-24 12:28:43 +0300

    feat: add source format choice and HEIC to JPEG convertion

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module dharlequin/bmp-to-png-converter
 go 1.17
 
 require golang.org/x/image v0.0.0-20211028202545-6944b10bf410
+
+require github.com/jdeng/goheif v0.0.0-20200323230657-a0d6a8b3e68f // indirect
--- main.go
+++ main.go
@@ -3,18 +3,23 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"image"
+	"image/jpeg"
 	"image/png"
 	"io/ioutil"
 	"log"
 	"os"
 	"strings"
 
+	"github.com/jdeng/goheif"
 	"golang.org/x/image/bmp"
 )
 
 func main() {
 	var srcDir string
 	var convDir string
+
+	var srcFormat string
 
 	fmt.Println("Enter source directory")
 	scanner := bufio.NewScanner(os.Stdin)
@@ -25,13 +30,16 @@ func main() {
 	scanner.Scan()
 	convDir = scanner.Text()
 
-	if !strings.HasSuffix(srcDir, "/") {
-		srcDir += "/"
-	}
+	srcDir = normalizeFolderName(srcDir)
+	convDir = normalizeFolderName(convDir)
 
-	if !strings.HasSuffix(convDir, "/") {
-		convDir += "/"
-	}
+	fmt.Println("Choose source format:")
+	fmt.Println("1 - BMP")
+	fmt.Println("2 - HEIC")
+	scanner.Scan()
+	srcFormat = scanner.Text()
+
+	srcFormat = assignFormat(srcFormat)
 
 	files, err := ioutil.ReadDir(srcDir)
 	handleError(err)
@@ -41,7 +49,7 @@ func main() {
 	var counter = 0
 
 	for _, f := range files {
-		if !f.IsDir() {
+		if !f.IsDir() && strings.HasSuffix(f.Name(), srcFormat) {
 			counter++
 
 			fmt.Printf("File #%d\n", counter)
@@ -51,17 +59,14 @@ func main() {
 			oldFile, err := os.Open(srcDir + f.Name())
 			handleError(err)
 
-			img, err := bmp.Decode(oldFile)
-			handleError(err)
+			img := decodeImage(oldFile, srcFormat)
 
-			newName := strings.Replace(f.Name(), "bmp", "png", -1)
-			// fmt.Println(newName)
+			newName := setNewFileName(f.Name(), srcFormat)
 
 			newFile, err := os.Create(convDir + newName)
 			handleError(err)
 
-			err = png.Encode(newFile, img)
-			handleError(err)
+			encodeImage(newFile, img, srcFormat)
 
 			os.Chtimes(convDir+newName, f.ModTime(), f.ModTime())
 		}
@@ -73,6 +78,75 @@ func main() {
 func handleError(err error) {
 	if err != nil {
 		log.Fatal(err)
+		os.Exit(100)
+	}
+}
+
+func normalizeFolderName(name string) string {
+	if !strings.HasSuffix(name, "/") {
+		name += "/"
+	}
+
+	return name
+}
+
+func assignFormat(format string) string {
+	switch format {
+	case "1":
+		return "bmp"
+	case "2":
+		return "HEIC"
+	default:
+		log.Fatal("Incorrect value received")
+		os.Exit(100)
+	}
+
+	return ""
+}
+
+func decodeImage(srcFile *os.File, srcFormat string) image.Image {
+	switch srcFormat {
+	case "bmp":
+		img, err := bmp.Decode(srcFile)
+		handleError(err)
+
+		return img
+	case "HEIC":
+		img, err := goheif.Decode(srcFile)
+		handleError(err)
+		return img
+	default:
+		log.Fatal("Incorrect value received")
+		os.Exit(100)
+	}
+
+	return nil
+}
+
+func setNewFileName(name string, format string) string {
+	switch format {
+	case "bmp":
+		return strings.Replace(name, format, "png", -1)
+	case "HEIC":
+		return strings.Replace(name, format, "jpeg", -1)
+	default:
+		log.Fatal("Incorrect value received")
+		os.Exit(100)
+	}
+
+	return ""
+}
+
+func encodeImage(file *os.File, image image.Image, srcFormat string) {
+	switch srcFormat {
+	case "bmp":
+		err := png.Encode(file, image)
+		handleError(err)
+	case "HEIC":
+		err := jpeg.Encode(file, image, nil)
+		handleError(err)
+	default:
+		log.Fatal("Incorrect value received")
 		os.Exit(100)
 	}
 }
